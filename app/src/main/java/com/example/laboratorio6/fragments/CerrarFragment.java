7633155ae1a25b494e192e4fee9c06672fd9bd81
LuisAngel <a20192832@pucp.edu.pe>
2024-06-08 17:07:57 -0500

    buscando huella digital

--- app/src/main/java/com/example/laboratorio6/fragments/CerrarFragment.java
+++ app/src/main/java/com/example/laboratorio6/fragments/CerrarFragment.java
@@ -1,25 +1,43 @@
 package com.example.laboratorio6.fragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
+import com.example.laboratorio6.MainActivity;
 import com.example.laboratorio6.databinding.FragmentCerrarBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class CerrarFragment extends Fragment {
 
     private FragmentCerrarBinding binding;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         binding = FragmentCerrarBinding.inflate(inflater, container, false);
+
+        // Initialize Firebase Auth
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+
+        if (currentUser != null) {
+            String userEmail = currentUser.getEmail();
+            binding.textViewUserEmail.setText(userEmail);
+        }
+
+        binding.btnLogout.setOnClickListener(v -> showLogoutConfirmationDialog());
+
         return binding.getRoot();
     }
 
@@ -27,5 +45,22 @@ public class CerrarFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void showLogoutConfirmationDialog() {
+        new AlertDialog.Builder(getContext())
+                .setTitle("Confirmación de Salida")
+                .setMessage("¿Estás seguro de que quieres salir?")
+                .setPositiveButton("Aceptar", (dialog, which) -> logout())
+                .setNegativeButton("Cancelar", null)
+                .show();
+    }
+
+    private void logout() {
+        firebaseAuth.signOut();
+        Intent intent = new Intent(getContext(), MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        getActivity().finish();
     }
 }
